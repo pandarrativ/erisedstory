07abea9cad7aba07ef9a1069c417f1291481e56b
huskyhehe <zhou.he1@northeastern.edu>
2024-02-22 00:16:07 -0500

    Add backend template

--- backend/server.ts
+++ backend/server.ts
@@ -2,8 +2,10 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
 app.use(cors());
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({ message: "hello" });
