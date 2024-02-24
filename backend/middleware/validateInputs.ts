23330fe907c1c991e313f28ce7fcd3a1c1bcf279
huskyhehe <zhou.he1@northeastern.edu>
2024-02-24 04:14:53 -0500

    Add validation for login inputs

--- backend/middleware/validateInputs.ts
+++ backend/middleware/validateInputs.ts
@@ -1,24 +1,37 @@
 import { body } from "express-validator";
+import ERRORS from "../errors";
 
 export function validateRegisterInputs() {
   return [
     body("role")
-      .notEmpty().withMessage("Role is required"),
+      .notEmpty().withMessage(ERRORS.ROLE_REQUIRED),
     body("email")
-      .notEmpty().withMessage("Email is required")
+      .notEmpty().withMessage(ERRORS.EMAIL_REQUIRED)
       .if((value: string) => !!value)
-      .isEmail().withMessage("Invalid email format"),
+      .isEmail().withMessage(ERRORS.INVALID_EMAIL),
     body("password")
-      .notEmpty().withMessage("Password is required")
+      .notEmpty().withMessage(ERRORS.PASSWORD_REQUIRED)
       .if((value: string) => !!value)
-      .isLength({ min: 5 }).withMessage("Password must be at least 5 characters long"),
+      .isLength({ min: 5 }).withMessage(ERRORS.PASSWORD_LENGTH),
     body("confirmPassword")
-      .notEmpty().withMessage("Confirm Password is required")
+      .notEmpty().withMessage(ERRORS.CONFIRM_PASSWORD_REQUIRED)
       .custom((value: string, { req }) => {
         if (req.body.password && value && value !== req.body.password) {
-          throw new Error("Passwords do not match");
+          throw new Error(ERRORS.PASSWORDS_MISMATCH);
         }
         return true;
       }),
+  ];
+}
+
+export function validateLoginInputs() {
+  return [
+    body("email")
+      .notEmpty()
+      .withMessage(ERRORS.EMAIL_REQUIRED)
+      .if((value: string) => !!value)
+      .isEmail()
+      .withMessage(ERRORS.INVALID_EMAIL),
+    body("password").notEmpty().withMessage(ERRORS.PASSWORD_REQUIRED),
   ];
 }
