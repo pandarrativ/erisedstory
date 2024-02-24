4e710680414b41a0721dba36fc623f0c337f41b8
Yaohong Xiang <xiang.yao@northeastern.edu>
2024-02-23 22:15:04 -0500

    fix delete

--- backend-django/api/utils/image_util.py
+++
@@ -1,3 +0,0 @@
-import random
-def get_image(Script_class):
-    return Script_class.image_path + str(random.randint(1, Script_class.image_counts)) + ".png"
--- backend-django/gunicorn_config.py
+++
@@ -1,5 +0,0 @@
-wsgi_app = "backend.wsgi:application"
-bind = "0.0.0.0:8000"  # Set the IP and port for your Django application
-workers = 4  # Adjust the number of workers based on your server's resources
-timeout = 120
-daemon = True
