7776a8625790b006d64db35af5a83844c0988b9e
Yaohong Xiang <xiang.yao@northeastern.edu>
2024-02-24 12:53:50 -0500

    chage backend api call for openai

--- backend-django/api/service/gpt.py
+++ backend-django/api/service/gpt.py
@@ -18,11 +18,57 @@ from ..scripts.ThreeBodyConfig import ThreeBody
 
 
 class GptAPI:
+    # load local parameters
     load_dotenv()
-    # openai.api_type = os.getenv("OPENAI_API_TYPE")
-    # openai.api_base = os.getenv("OPENAI_API_BASE")
-    # openai.api_version = os.getenv("OPENAI_API_VERSION")
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+    client = openai.AzureOpenAI(
+        api_key = os.getenv("OPENAI_API_KEY"),
+        api_version = os.getenv("OPENAI_API_VERSION"),
+        azure_endpoint = os.getenv("OPENAI_API_ENDPOINT"),
+    )
+    
+ ##############################################################################################################################################
+ ## Below are the functions to set openai model parameters and send to openai
+ ##############################################################################################################################################
+    @staticmethod
+    def send_prompt(prompt, model = "gpt-35-turbo", is_json=False):
+        if is_json:
+            response = OpenAIAPI.client.chat.completions.create(
+                model= model, # model = "deployment_name".
+                response_format={ "type": "json_object" },
+                messages=[{"role": "user", "content": prompt}]
+            )
+        else:
+            response = OpenAIAPI.client.chat.completions.create(
+                model= model, # model = "deployment_name".
+                messages=[{"role": "user", "content": prompt}]
+            )
+        
+        resp =response.choices[0].message.content
+        return resp
+
+    @staticmethod
+    def send_messages(messages, model = "gpt-35-turbo", is_json=False):
+        if is_json:
+            response = OpenAIAPI.client.chat.completions.create(
+                model= model, 
+                response_format={ "type": "json_object" },
+                messages=messages
+            )
+        else:
+            response = OpenAIAPI.client.chat.completions.create(
+                model= model, 
+                messages=messages
+            )
+        
+        resp =response.choices[0].message.content
+        return resp
+
+ ##############################################################################################################################################
+ ## Below are the functions to for the main function
+ ##############################################################################################################################################
+ 
+
+
     """
     prompt state: 
         0: ask for a story description
@@ -39,12 +85,12 @@ class GptAPI:
 
         # set prompt script
         Script_class = HarryPotter
-        if script_name == "Shantaram":
-            Script_class = Shantaram
-        elif script_name == "Witcher":
-            Script_class = Witcher
-        elif script_name == "ThreeBody":
-            Script_class = ThreeBody
+        # if script_name == "Shantaram":
+        #     Script_class = Shantaram
+        # elif script_name == "Witcher":
+        #     Script_class = Witcher
+        # elif script_name == "ThreeBody":
+        #     Script_class = ThreeBody
             
         username = Script_class.username
 
@@ -56,8 +102,8 @@ class GptAPI:
             
         
         ##############################################3##################33
-        # this is temp
-        first_story_image = Script_class.first_story_image
+        # First image, disable in children system
+        # first_story_image = Script_class.first_story_image
         ###################################################################
         
 
@@ -72,14 +118,12 @@ class GptAPI:
 
             # generate img
             ###############################################################################
-            # img = DalleAPI.generateImg(res["story"])
-            res["img"] = get_image(Script_class=Script_class)
-            # res["img"] = first_story_image
+            # res["img"] = get_image(Script_class=Script_class)
             ###############################################################################
 
             #store data
             # ScriptPlayDAO.insert_story_description(scriptplay_id, order, res["story"], res["img"])
-            ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="story-description",data = res)
+            # ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="story-description",data = res)
             return res
         
         # ask for options
@@ -93,17 +137,14 @@ class GptAPI:
 
             # generate img
             ###############################################################################
-            # img = DalleAPI.generateImg(res["story"])
-            res["img"] = get_image(Script_class=Script_class)
-            # res["img"] = first_story_image
+            # res["img"] = get_image(Script_class=Script_class)
             ###############################################################################
-
 
 
             #store data
             # ScriptPlayDAO.insert_story_description(scriptplay_id, order, res["story"], res["img"])
             # ScriptPlayDAO.create_options(scriptplay_id, order+1, res["question"], res["option_1"], res["option_2"], res["option_3"])
-            ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="generate-options",data = res)
+            # ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="generate-options",data = res)
             return res
         
         # ask for character
@@ -116,23 +157,22 @@ class GptAPI:
                 res = parse_ask_character_to_json(resp)
             
             res["chat_background"] = PromptTemplate.get_chat_background(username, res["character_name"], res["character_personality"], prompt)
-            res["avatar"] = get_random_avatar()
-            # generate img
-            ###############################################################################
-            # generate img
-            # img = DalleAPI.generateImg(res["story"])
-            res["sm_bg_img"] = "/sample/sample_sm_bg.png"
+            # res["avatar"] = get_random_avatar()
+            # # generate img
+            # ###############################################################################
+            # # generate img
+            # # img = DalleAPI.generateImg(res["story"])
+            # res["sm_bg_img"] = "/sample/sample_sm_bg.png"
 
-            for image, i in zip(get_random_post_imgs(), [1,2,3]):
-                res["post_image_" + str(i)] = image
-            ###############################################################################
-            res["img"] = get_image(Script_class=Script_class)
-            # res["img"] = first_story_image
+            # for image, i in zip(get_random_post_imgs(), [1,2,3]):
+            #     res["post_image_" + str(i)] = image
+            # ###############################################################################
+            # res["img"] = get_image(Script_class=Script_class)
             
             #store data
             # ScriptPlayDAO.insert_story_description(scriptplay_id, order, res["story"], res["img"])
             # ScriptPlayDAO.start_conversation(scriptplay_id, order+1, res["character_name"], res["character_personality"], res["chat_background"], res["first_sentence"])
-            ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="generate-character",data = res)
+            # ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="generate-character",data = res)
             return res
         
         #continue options
@@ -144,19 +184,17 @@ class GptAPI:
                 resp = GptAPI.send_prompt(prompt + " " +PromptTemplate.continue_options(username, req["choice"]))
                 res = parse_to_json(resp)
 
-
             # generate img
             ###############################################################################
-            # img = DalleAPI.generateImg(res["story"])
-            res["img"] = get_image(Script_class=Script_class)
-            # res["img"] = first_story_image
+            # res["img"] = get_image(Script_class=Script_class)
             ###############################################################################
+
             
             #store data
             # ScriptPlayDAO.choose_option(scriptplay_id, order, req["choice"])
             # ScriptPlayDAO.insert_story_description(scriptplay_id, order, res["story"], res["img"])
-            ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="continue-options",data = res)
-            ScriptPlayDAO.add_option_choice(object_id=scriptplay_id, order=order-1,choice=req["choice"])
+            # ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="continue-options",data = res)
+            # ScriptPlayDAO.add_option_choice(object_id=scriptplay_id, order=order-1,choice=req["choice"])
             return res
         
 
@@ -173,54 +211,18 @@ class GptAPI:
             
             # generate img
             ###############################################################################
-            # img = DalleAPI.generateImg(res["story"])
-            res["img"] = get_image(Script_class=Script_class)
-            # res["img"] = first_story_image
+            # res["img"] = get_image(Script_class=Script_class)
             ###############################################################################
+
 
 
             #store data
             # ScriptPlayDAO.insert_story_description(scriptplay_id, order, res["story"], res["img"])
             # ScriptPlayDAO.end_convseration(scriptplay_id, order)
-            ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="continue-character",data = res)
-            ScriptPlayDAO.add_character_conversation(object_id = scriptplay_id, order = order-1,messages = messages)
+            # ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="continue-character",data = res)
+            # ScriptPlayDAO.add_character_conversation(object_id = scriptplay_id, order = order-1,messages = messages)
             return res
         
-        elif prompt_state == "5":
-            try:
-                resp = GptAPI.send_prompt(prompt + " " + PromptTemplate.ask_groupchat(username=username))
-                res = parse_ask_character_to_json(resp)
-            except:
-                resp = GptAPI.send_prompt(prompt + " " + PromptTemplate.ask_groupchat(username=username))
-                res = parse_ask_character_to_json(resp)
-            
-            res["img"] = get_image(Script_class=Script_class)
-            # res["img"] = first_story_image
-            res["character_list"] = get_random_avatars(res["character_list"])
-
-            ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="generate-groupchat",data = res)
-            return res
-    
-    # continue story after group chat
-        elif prompt_state == "6":
-            # make a summary of conversation
-            messages, character_name = req["messages"], req["character_name"]
-            try:
-                resp = GptAPI.send_prompt(prompt + " " + PromptTemplate.end_groupchat(messages, username))
-                res = parse_to_json(resp)
-            except:
-                resp = GptAPI.send_prompt(prompt + " " + PromptTemplate.end_groupchat(messages, username))
-                res = parse_to_json(resp)
-            
-
-            res["img"] = get_image(Script_class=Script_class)
-            # res["img"] = first_story_image
-
-            #store data
-            ScriptPlayDAO.add_gameplay_data(object_id = scriptplay_id, order = order, event_type="continue-groupchat",data = res)
-            ScriptPlayDAO.add_groupchat_conversation(object_id = scriptplay_id, order = order-1,messages = messages)
-            return res
-
     
     @staticmethod
     def chatGPT_conversation(req):
@@ -276,69 +278,6 @@ class GptAPI:
         ))
         res = parse_ask_character_to_json(resp)
         return res
-
-
-
-
-
-
-
- ##############################################################################################################################################
- ##############################################################################################################################################
-
-
-    # @staticmethod
-    # def send_prompt(prompt, engine = "gpt-35-turbo", temperature=0.7, max_tokens=3000,top_p=0.95,frequency_penalty=0,presence_penalty=0,stop=None):
-    #     completion = openai.ChatCompletion.create(
-    #         engine=engine,
-    #         messages = [{"role":"user","content":prompt}],
-    #         temperature=temperature,
-    #         max_tokens=max_tokens,
-    #         top_p=top_p,
-    #         frequency_penalty=frequency_penalty,
-    #         presence_penalty=presence_penalty,
-    #         stop=stop
-    #     )
-        
-    #     print(prompt)
-    #     resp =completion.choices[0].message.content
-    #     print("-------------------resp---------------")
-    #     print(resp)
-    #     print("-------------------resp---------------")
-    #     return resp
-
-    # @staticmethod
-    # def send_messages(messages, engine = "gpt-35-turbo", temperature=0.7, max_tokens=3000,top_p=0.95,frequency_penalty=0,presence_penalty=0,stop=None):
-    #     completion = openai.ChatCompletion.create(
-    #         engine=engine,
-    #         messages = messages,
-    #         temperature=temperature,
-    #         max_tokens=max_tokens,
-    #         top_p=top_p,
-    #         frequency_penalty=frequency_penalty,
-    #         presence_penalty=presence_penalty,
-    #         stop=stop
-    #     )
-        
-    #     resp = completion.choices[0].message.content
-    #     return resp
-
-
-    @staticmethod
-    def send_prompt(prompt):
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", 
-                                                                                "content": prompt
-                                                                            }])
-        resp =completion.choices[0].message.content
-
-        return resp
- 
- 
-    @staticmethod
-    def send_messages(messages):
-        completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-        resp =completion.choices[0].message.content
-        return resp
 
 
 
