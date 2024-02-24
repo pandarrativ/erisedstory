4dd321050db25ee06afe6e14b166512189c87dad
huskyhehe <zhou.he1@northeastern.edu>
2024-02-24 04:17:27 -0500

    Add login controller

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -1,7 +1,9 @@
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import { validationResult } from "express-validator";
 import { User, UserModel } from "../models/user";
+import ERRORS from "../errors";
 
 const authController = {
   async registerUser(req: Request, res: Response) {
@@ -18,13 +20,13 @@ const authController = {
     }
 
     if (role === "admin") {
-      res.status(403).json({ error: "Registering as admin is not allowed" });
+      res.status(403).json({ error: ERRORS.ACCESS_FORBIDDEN });
       return;
     }
 
     const emailAlreadyRegistered = await UserModel.findOne({ email });
     if (emailAlreadyRegistered) {
-      res.status(403).json({ error: "Email already registered" });
+      res.status(409).json({ error: ERRORS.EMAIL_IN_USE });
       return;
     }
 
@@ -33,14 +35,48 @@ const authController = {
       user: {
         email: user.email,
         username: user.username,
-        role: user.role,
       },
-      message: "User registered successfully",
+      message: "Successfully registered",
+    });
+  },
+
+  async loginUser(req: Request, res: Response) {
+    const { email, password } = req.body;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const formattedErrors = errors.array().reduce((acc: { [key: string]: string }, error: any) => {
+        acc[error.path] = error.msg;
+        return acc;
+      }, {});
+      res.status(400).json({ errors: formattedErrors });
+      return;
+    }
+
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      res.status(401).json({ error: ERRORS.EMAIL_NOT_REGISTERED });
+      return;
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.hashedPassword);
+    if (!passwordMatch) {
+      res.status(401).json({ error: ERRORS.INCORRECT_PASSWORD });
+      return;
+    }
+
+    const token = createJWT(user);
+    res.cookie("token", token, { httpOnly: true, secure: true });
+    res.status(200).json({
+      user: {
+        email: user.email,
+        username: user.username,
+      },
+      message: "Successfully logged in",
     });
   },
 };
 
-///////////////////////////////////////////////////////////////////
 async function createUser(email: string, password: string, role: string) {
   try {
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -54,6 +90,14 @@ async function createUser(email: string, password: string, role: string) {
   } catch (error) {
     throw error;
   }
+}
+
+function createJWT(user: User) {
+  return jwt.sign(
+    { userId: user._id }, 
+    process.env.JWT_SECRET_KEY!, 
+    { expiresIn: process.env.JWT_LIFETIME }
+  );
 }
 
 export default authController;
