de5cfb82f2317c5c76ab824b463e23c6c0c1f814
huskyhehe <zhou.he1@northeastern.edu>
2024-02-24 04:17:00 -0500

    Update env settings

--- backend/server.ts
+++ backend/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
 import swaggerUi from "swagger-ui-express";
 import connectDb from "./db/conn";
 import { swaggerSpec } from "./configs/swaggerConfig";
@@ -7,7 +8,7 @@ import authRoutes from "./routes/authRoutes";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = "mongodb://127.0.0.1:27017";
+const MONGODB_URI = process.env.MONGODB_URI;
 
 app.use(cors());
 app.use(express.json());
@@ -22,6 +23,9 @@ app.use("/api/v1/auth", authRoutes);
 
 const start = async () => {
   try {
+    if (!MONGODB_URI) {
+      throw new Error("MongoDB URI is required");
+    }
     await connectDb(MONGODB_URI);
     app.listen(PORT, () => {
       console.log(`Listening on http://localhost:${PORT}.`);
