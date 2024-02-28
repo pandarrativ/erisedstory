1eb8874340f79d8db9724b251071173570f5802a
huskyhehe <zhou.he1@northeastern.edu>
2024-02-28 16:25:23 -0500

    Update errors

--- backend/errors.ts
+++ backend/errors.ts
@@ -1,15 +1,17 @@
 const ERRORS = {
   ROLE_REQUIRED: "roleRequired",
+  INVALID_ROLE: "invalidRole",
   EMAIL_REQUIRED: "emailRequired",
   INVALID_EMAIL: "invalidEmailFormat",
+  EMAIL_IN_USE: "emailAlreadyInUse",
+  EMAIL_NOT_REGISTERED: "emailNotRegistered",
   PASSWORD_REQUIRED: "passwordRequired",
   PASSWORD_LENGTH: "passwordLength",
   CONFIRM_PASSWORD_REQUIRED: "confirmPasswordRequired",
   PASSWORDS_MISMATCH: "passwordMismatch",
-  EMAIL_IN_USE: "emailAlreadyInUse",
-  EMAIL_NOT_REGISTERED: "emailNotRegistered",
   INCORRECT_PASSWORD: "incorrectPassword",
   ACCESS_FORBIDDEN: "accessForbidden",
+  UNAUTHENTICATED: "unauthenticated",
 };
 
 // TODO: this is for frontend message mapping
@@ -24,6 +26,7 @@ const MESSAGES = {
   [ERRORS.EMAIL_IN_USE]: "Email already registered",
   [ERRORS.EMAIL_NOT_REGISTERED]: "Email not registered",
   [ERRORS.ACCESS_FORBIDDEN]: "Access denied",
+  [ERRORS.UNAUTHENTICATED]: "unauthenticated",
 };
 
 export default ERRORS;
