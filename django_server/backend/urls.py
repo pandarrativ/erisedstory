93b0daebc23f317dea90f6d21365c87308ce29f6
Yaohong Xiang <xiang.yao@northeastern.edu>
2024-05-20 12:37:36 -0500

    update

--- django_server/backend/urls.py
+++ django_server/backend/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path("pdtalk/", include("pdtalk.urls")),
+    path("erised/", include("erised.urls")),
 ]
---
+++ django_server/erised/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    # path("story", StoryPlayApiView.as_view()),
+]
